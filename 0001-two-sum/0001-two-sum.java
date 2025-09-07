ef353a5f522f258df38d835633c2cac982d07793
Saswati Samal <saswatisamal02@gmail.com>
2025-09-07 16:14:57 +0530

    Time: 13 ms (49.97%), Space: 45.4 MB (5.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,31 @@
  import java.util.Hashtable;
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-      HashMap<Integer,Integer> hm= new HashMap();
-      int rem=0;
-      for(int i=0;i<nums.length;i++){
-        rem=target-nums[i];
-        if(hm.containsKey(rem)){
-            return new int[]{hm.get(rem),i};
-        }else{
-            hm.put(nums[i],i);
+    static class Pair{
+        int num;
+        int index;
+        Pair(int num,int index){
+            this.num=num;
+            this.index=index;
         }
-      }
-      return new int[]{-1,-1};
     }
+    public int[] twoSum(int[] nums, int target) {
+        Pair[] numWithIndex= new Pair[nums.length];
+        for( int i=0;i<nums.length;i++){
+            numWithIndex[i]=new Pair(nums[i],i);
+        }
+        Arrays.sort(numWithIndex, Comparator.comparing(p->p.num));
+        int left=0,right=numWithIndex.length-1;
+        int result=0;
+        while(left<right) {
+            result = numWithIndex[left].num + numWithIndex[right].num;
+            if (result == target) {
+                return new int[]{numWithIndex[left].index, numWithIndex[right].index};
+            } else if (result < target) {
+                left++;
+            } else if (result > target) {
+                right--;
+            }
+        }
+        return new int[]{-1,-1};
+        }
 }
